c08fcfbc38c3acc81e9c6f3e34e44cb38df99a3d
Marcin Wrochna <marcin.wrochna@mim.ai>
2024-02-03 23:06:55 +0000

    Add explainer testing, clean-up explainer a bit.

--- T2T_ViT/vit_shapley/CIFAR_10_Dataset.py
+++ T2T_ViT/vit_shapley/CIFAR_10_Dataset.py
@@ -21,24 +21,29 @@ def apply_masks_to_batch(
 
     Args:
     - images: shape (B, C, H, W).
-    - masks: shape (B, n_masks_per_image, n_players).
+    - masks: shape (B, n_masks_per_image, n_players) or (B, n_players).
     - labels: shape (B,).
 
     Returns (images, labels, masks) where:
     - images: masked to shape (B * n_masks_per_image, C, H, W).
-    - labels: repeated to shape (B * n_masks_per_image,).
     - masks: reshaped to shape (B * n_masks_per_image, n_players).
+    - labels: repeated to shape (B * n_masks_per_image,).
     """
     if len(masks.shape) == 2:
         masks = masks.unsqueeze(dim=1)
+
     B, n_masks_per_image, n_players = masks.shape
-    assert images.shape[0] == B and images.shape[1] in [1, 3], f"Unexpected {images.shape=}"
-    if labels:
+
+    images_masked = apply_masks(images, masks)
+
+    if labels is not None:
         assert labels.shape == (B,)
         labels = labels.repeat_interleave(n_masks_per_image)
-    
-    images_masked = apply_masks(images, masks)
+    else:
+        labels = torch.zeros((B * n_masks_per_image,), dtype=torch.long)
+
     masks = masks.view(B * n_masks_per_image, n_players)
+
     return images_masked, masks, labels
 
 
@@ -57,7 +62,8 @@ def apply_masks(images: torch.Tensor, masks: torch.Tensor) -> torch.Tensor:
     """
     B, C, H, W = images.shape
     B2, n_masks_per_image, n_players = masks.shape
-    assert B == B2
+    assert B == B2, f"Expected same batch dim, got {images.shape=} {masks.shape=}"
+    assert C in [1, 3], f"Expected 1 or 3 channels, got {images.shape=}"
 
     mask_H = int(np.round(np.sqrt(n_players)))
     mask_W = mask_H
@@ -264,15 +270,15 @@ def _tensor_to_image(
     assert len(x.shape) == 3, f"Expected shape (C, H, W), got {x.shape}"
     C, H, W = x.shape
     assert C in [1, 3], f"Expected 1 or 3 channels, got shape {x.shape}"
-    img = x.permute(1, 2, 0).numpy()  # shape (H, W, C)
+    img = x.permute(1, 2, 0).cpu().numpy()  # shape (H, W, C)
     if mean is None or std is None:
         vmin, vmax = img.min(), img.max()
-        img = (img - vmin) / (vmax - vmin)
+        img = (img - vmin) / (vmax - vmin + 1e-10)
     else:
         img = img * std + mean
     img = np.clip(img * 255, 0, 255).astype("uint8")
     pil_image = PIL.Image.fromarray(img, mode="RGB")
-    pil_image.thumbnail((int(scale * W), int(scale * H)), PIL.Image.Resampling.NEAREST)
+    pil_image = pil_image.resize((int(scale * W), int(scale * H)), PIL.Image.Resampling.NEAREST)
     return pil_image
 
 
@@ -296,7 +302,9 @@ def _make_image_grid(
     labels: Optional[list[str]] = None,
     colors: Optional[list[tuple[int, int, int]]] = None,
     n_columns: int = 8,
-    font_size=19,
+    font_size: int = 19,
+    border: int = 1,
+    border_color: tuple[int, int, int] = (0, 0, 0),
 ) -> PIL.Image.Image:
     """Arrange a list of images into a grid image."""
     B = len(images)
@@ -314,8 +322,9 @@ def _make_image_grid(
 
     # Paste onto grid.
     # Note that Pillow swaps H and W.
-    scaledH, scaledW = max(img.size[1] for img in images), max(img.size[0] for img in images)
-    grid = PIL.Image.new("RGB", (n_columns * scaledW, n_rows * scaledH))
+    H, W = max(img.size[1] for img in images), max(img.size[0] for img in images)
+    H, W = H + border, W + border
+    grid = PIL.Image.new("RGB", (n_columns * W, n_rows * H), color=border_color)
     for i, im in enumerate(images):
-        grid.paste(im, ((i % n_columns) * scaledW, (i // n_columns) * scaledH))
+        grid.paste(im, ((i % n_columns) * W, (i // n_columns) * H))
     return grid
