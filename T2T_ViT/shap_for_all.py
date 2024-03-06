320cf297431b9196f514a80e36d32c7b0223c8d6
jacek-tomasz-rutkowski <jr371580@students.mimuw.edu.pl>
2024-03-06 22:37:21 +0000

    added shapes and check sum shap values

--- T2T_ViT/shap_for_all.py
+++ T2T_ViT/shap_for_all.py
@@ -3,9 +3,8 @@ import scipy.special
 from tqdm import tqdm
 
 import models.t2t_vit
-from utils import load_checkpoint
 from vit_shapley.CIFAR_10_Dataset import PROJECT_ROOT, CIFAR_10_Datamodule, apply_masks_to_batch
-
+from vit_shapley.modules.surrogate import Surrogate
 
 class Shap_values():
     def __init__(self, model: torch.nn.Module,
@@ -26,7 +25,7 @@ class Shap_values():
         """
         K is equal to num_players or to num_players or num_players- 1
         returns all 0-1 masks shape (2**K, K)
-        and expands to shape (b, 2**K, K)
+        and expands to shape (B, 2**K, K)
         """
         K, N = K, 1
         masks = (torch.bitwise_and (torch.arange (2**(K*N)).unsqueeze (-1), \
@@ -43,8 +42,7 @@ class Shap_values():
         image and mask
         image is tensor of shape (B, C, H, W) - batch of images from the dataset
         returns: values of the model on images of size (B*(2**num_players), C, H, W),
-        shape of output is (B*2**num_players, num_classes),
-        # after reshape it is (B, 2**num_players, num_classes)
+        shape of output is (B, 2**num_players, num_classes)
         """
 
         # batch version
@@ -59,27 +57,30 @@ class Shap_values():
                                 masks=masks_.to(self.device))
             masks_.cpu().detach()
             value = self.model(pictures_).cpu().detach().view(self.batch_size, -1, self.num_classes)
+            # (B, max_batch, num_classes)
             # we have to resize before appending
             # for later concatenation
             values.append(value)
 
         return torch.cat(values, dim=1)
-        # we have to permute the values?
-        #.view(-1, self.batch_size, self.num_classes).permute(1,0,2)
+        
+
 
     def compute_dif_for_feature(self, model_values: torch.Tensor, player_number: int):
-        # tutaj różnica jest jakby po złym wymiarze -
-        # powinna być po środkowym [:, dim, :], a jest po ostatnim [:, :, dim]
+        
         masks = self.get_all_masks(K=self.num_players)
+        # (B, 2**num_players, num_players)
 
         player_mask = (masks[:, :, player_number] == 1).flatten(0,1)
+        # (B*2**num_players,)
 
         other_coalitions_mask = ~(player_mask)
 
         model_values = model_values.view(-1, model_values.shape[-1])
-        # shape b*2**num_players, num_players
+        # (b*2**num_players, num_players)
 
         dif = model_values[player_mask] - model_values[other_coalitions_mask]
+        # (B*2**(num_players - 1), num_classes)
 
         return dif.view(self.batch_size, -1, self.num_classes)
 
@@ -89,33 +90,47 @@ class Shap_values():
         and given feature number
         """
         almost_all_masks = self.get_all_masks(self.num_players - 1)
-        covered = torch.sum(almost_all_masks, dim=-1)
+        # (B, 2*(num_players - 1), num_players - 1)
 
-        # shap_vs = torch.zeros(self.num_players, self.batch_size, self.num_classes)
+        covered = torch.sum(almost_all_masks, dim=-1)
+        # (B, 2*(num_players - 1))
 
         shap_vs = torch.zeros(self.batch_size, self.num_players, self.num_classes)
 
         all_masks = self.get_all_masks(self.num_players)
+        # (B, 2*(num_players), num_players)
         model_values = self.compute_model_on_patches(all_masks)
+        # (B, 2**num_players, num_classes)
+
         values = []
         for player_number in tqdm(range(self.num_players), desc='compute shapley values'):
             values = self.compute_dif_for_feature(model_values, player_number)
-            # print(f'values shape is {values.shape}')
+            # (B*2**(num_players - 1), num_classes)
 
             for i in range(values.shape[1]):
                 shap_vs[:, player_number] += values[:, i]/scipy.special.binom(self.num_players-1, covered[:, i].unsqueeze(1))
 
-        print(f'Shapley values are: {shap_vs/self.num_players}')
         return shap_vs/self.num_players
 
 
 def main() -> None:
+    # target_model = models.t2t_vit.t2t_vit_14(num_classes=10)
+    # # target_model_path = PROJECT_ROOT / "saved_models/downloaded/cifar10/cifar10_t2t-vit_14_98.3.pth"
+    # target_model_path = PROJECT_ROOT / "saved_models/transferred/cifar10/ckpt_0.01_0.0005_97.5.pth"
+    # load_checkpoint(target_model_path, target_model, device='cuda')
     target_model = models.t2t_vit.t2t_vit_14(num_classes=10)
-    # target_model_path = PROJECT_ROOT / "saved_models/downloaded/cifar10/cifar10_t2t-vit_14_98.3.pth"
-    target_model_path = PROJECT_ROOT / "saved_models/transferred/cifar10/ckpt_0.01_0.0005_97.5.pth"
-    load_checkpoint(target_model_path, target_model, device='cuda')
+    # target_model_path = PROJECT_ROOT / "saved_models/transferred/cifar10/ckpt_0.01_0.0005_97.5.pth"
+    # load_checkpoint(target_model_path, target_model)
 
-    datamodule = CIFAR_10_Datamodule(num_players=9,
+    surrogate = Surrogate.load_from_checkpoint(
+        PROJECT_ROOT / "saved_models/surrogate/cifar10/_player16_lr1e-05_wd0.0_b256_epoch28.ckpt",
+        # PROJECT_ROOT / "saved_models/surrogate/cifar10/_player196_lr1e-05_wd0.0_b128_epoch49.ckpt",
+        target_model=target_model,
+        num_players=16
+    )
+
+
+    datamodule = CIFAR_10_Datamodule(num_players=16,
                                      num_mask_samples=1,
                                      paired_mask_samples=False,
                                      batch_size=3,
@@ -125,13 +140,12 @@ def main() -> None:
 
     images = data['images']
     labels = data['labels']
-    masks = data['masks']
     import time
 
     time_0 = time.time()
 
-    Shap_vs = Shap_values(target_model, images, labels,
-                          num_players=4)
+    Shap_vs = Shap_values(surrogate, images, labels,
+                          num_players=16)
 
     print(Shap_vs.shap_values())
 
